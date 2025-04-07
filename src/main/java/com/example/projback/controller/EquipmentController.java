f6845bcc341bd4ae5e25ea45c65d5cbc7348f594
Grzegorz Zieliński <GRZESIE2000@GMAIL.COM>
2025-04-07 12:41:06 +0200

    Revert "L10: added one example of 1. & 2."
    This reverts commit 2eafd39c2bf45bf12412af4b0a55c40e06f13f8f.

--- src/main/java/com/example/projback/controller/EquipmentController.java
+++ src/main/java/com/example/projback/controller/EquipmentController.java
@@ -3,7 +3,6 @@ package com.example.projback.controller;
 import com.example.projback.dto.MakeReservationDTO;
 import com.example.projback.entity.Equipment;
 //import com.example.projback.wzorce.L8.OdwracanieZaleznosci.IEquipmentService;
-import com.example.projback.wzorce.L10.EquipmentFilter;
 import com.example.projback.wzorce.L3.Proxy.EquipmentServiceProxy;
 import com.example.projback.wzorce.L8.OdwracanieZaleznosci.SegragacjaInterfejsow.Equipment.IEquipmentService_Query;
 import com.example.projback.wzorce.L8.OdwracanieZaleznosci.SegragacjaInterfejsow.Equipment.IEquipmentService_Creating;
@@ -13,7 +12,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/equipment")
@@ -91,12 +89,5 @@ public class EquipmentController {
 //        equipmentCreationService.createEquipmentByMediator(equipment, token);
 //        return "mediator";
     }
-
-    //###   start L10, EquipmentFilter (part 2)
-    @GetMapping("/filteredByPrice")
-    public List<Equipment> getFilteredEquipment(@RequestParam double minPrice) {
-        return equipmentServiceProxy.getEquipmentFilteredByPrice(minPrice);
-    }
-    //###   end L10, EquipmentFilter (part 2)
 }
 
--- src/main/java/com/example/projback/wzorce/L10/RoomFilter.java
+++
@@ -1,7 +0,0 @@
-package com.example.projback.wzorce.L10;
-
-import com.example.projback.entity.Room;
-
-public interface RoomFilter {
-    boolean test(Room room);
-}
