284b50dba1a2d9488676c51f3d7f0ba910519267
Grzegorz Zieliński <GRZESIE2000@GMAIL.COM>
2025-04-07 12:41:06 +0200

    Revert "L10: done"
    This reverts commit 1607236d30e575e33e6c522326371a8f9a20e2ef.

--- src/main/java/com/example/projback/controller/EquipmentController.java
+++ src/main/java/com/example/projback/controller/EquipmentController.java
@@ -92,11 +92,11 @@ public class EquipmentController {
 //        return "mediator";
     }
 
-    //###   start L10, EquipmentFilter (part 3)
+    //###   start L10, EquipmentFilter (part 2)
     @GetMapping("/filteredByPrice")
     public List<Equipment> getFilteredEquipment(@RequestParam double minPrice) {
         return equipmentServiceProxy.getEquipmentFilteredByPrice(minPrice);
     }
-    //###   end L10, EquipmentFilter (part 3)
+    //###   end L10, EquipmentFilter (part 2)
 }
 
--- src/main/java/com/example/projback/wzorce/L8/OdwracanieZaleznosci/SegragacjaInterfejsow/Room/IRoomService_Query.java
+++ src/main/java/com/example/projback/wzorce/L8/OdwracanieZaleznosci/SegragacjaInterfejsow/Room/IRoomService_Query.java
@@ -15,8 +15,5 @@ public interface IRoomService_Query {
     boolean isRoomAvailableIgnoringReservation(RoomAvailableIgnoringReservationDTO roomAvailableIgnoringReservationDTO);
     Room getRoomById(Long roomId, String token);
     void printAllRoom();
-    //###   start L10, RoomFilter  (part 3)
-    List<Room> getRoomsFilteredByHourlyRate(double minRate);
-    //###   start L10, RoomFilter  (part 3)
 }
 //###   end L8 Segregacja interfejsów
